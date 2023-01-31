9b6f2dcd0fe8dc2c88127608b2eb895486b18005
Vladyslav Klymenko <vladislaviwe@outlook.com>
2023-01-31 20:31:19 +0200

    hw2-express
    Task completed

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,70 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const {nanoid} = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const updateContacts = async(contacts) => await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-const removeContact = async (contactId) => {}
+  async function listContacts () {
+    const data = await fs.readFile(contactsPath)
 
-const addContact = async (body) => {}
+    return JSON.parse(data);
+  }
+  
+  async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const result = contacts.find(item => item.id === contactId);
+    return result || null;
+  }
+  
+  async function addContact({name, email, phone}) {
+    const contacts = await listContacts();
+    if (!name || !email || !phone) {
+      console.warn("One of the values is empty");
+      return null;
+    }
+    const newContact = {
+        id: nanoid(),
+        name,
+        email,
+        phone
+    }
 
-const updateContact = async (contactId, body) => {}
+    contacts.push(newContact);
+    await updateContacts(contacts);
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+    return newContact;
+  }
+
+  async function updateContact(contactId, data) {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(item => item.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+    contacts[index] = {contactId, ...data};
+    await updateContact(contacts);
+
+    return contacts[index];
+  }
+
+  async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(item => item.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await updateContacts(contacts);
+
+    return result;
+  }
+
+  module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+    updateContact,
+  }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,20 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const ctrl = require("../../controllers/contacts");
+const { ctrlWrapper } = require("../../helpers");
+const { validateContact } = require("../../middlewares");
+const schemas = require("../../schemas/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', ctrlWrapper(ctrl.listContacts));
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:contactId', ctrlWrapper(ctrl.getContactById));
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/', validateContact(schemas.addSchema), ctrlWrapper(ctrl.addContact));
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:contactId', ctrlWrapper(ctrl.removeContact));
+
+router.put('/:contactId',validateContact(schemas.addSchema), ctrlWrapper(ctrl.updateContact));
 
 module.exports = router
