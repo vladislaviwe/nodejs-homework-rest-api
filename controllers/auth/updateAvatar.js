8c77a17b4f5c0a1f2d3ab1425cd38c29ba4f4410
Vladyslav Klymenko <vladislaviwe@outlook.com>
2023-03-11 13:39:56 +0200

    Tasks completed

---
+++ controllers/auth/updateAvatar.js
@@ -0,0 +1,27 @@
+const fs = require("fs/promises");
+const path = require("path");
+const Jimp = require("jimp");
+
+const {User} = require("../../models/user");
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars")
+
+const updateAvatar = async(req, res) => {
+    const {path: tempUpload, originalname} = req.file;
+    const {_id, name} = req.user;
+
+    const avatar = await Jimp.read("temp/" + originalname);
+    avatar.resize(250,250).write("temp/" + originalname);
+
+    const newAvatarName = `${_id}_${name}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, newAvatarName);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("avatars", newAvatarName);
+    await User.findByIdAndUpdate(_id, {avatarURL});
+
+    res.json({
+        avatarURL,
+    })
+}
+
+module.exports = updateAvatar;
